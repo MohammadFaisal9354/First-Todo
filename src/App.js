13c888454a2ba72aeee21cbc31de55e559935b72
Mohammad Faisal <114982586+MohammadFaisal9354@users.noreply.github.com>
2023-07-15 08:06:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTodos([...todos, newTodo]);
+    setNewTodo('');
+  }
+
+  const handleDelete = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
+        <button type="submit">Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleDelete(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
